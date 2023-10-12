10ea75d5f02029a4ab1fc33e0f8292798c939d72
maximecornaton <maxime.cornaton@cpe.fr>
2023-10-12 11:49:59 +0200

    inventory

--- app/src/main/java/fr/pokemongeo/gr1/Pokestop.java
+++ app/src/main/java/fr/pokemongeo/gr1/Pokestop.java
@@ -1,5 +1,9 @@
 package fr.pokemongeo.gr1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Pokestop {
     private String name;
     private String description;
@@ -9,6 +13,28 @@ public class Pokestop {
         this.name = name;
         this.description = description;
         this.imageResource = imageResource;
+    }
+
+    public List<Item> getItems() {
+        List<Item> itemList = new ArrayList<>();
+        Random random = new Random();
+        int nbItems = random.nextInt(5) + 1;
+
+        for (int i = 0; i < nbItems; i++) {
+            int itemIndex = random.nextInt(3);
+            switch (itemIndex) {
+                case 0:
+                    itemList.add(new Item("Potion", "potion", "Restaure 20 PV", 3));
+                    break;
+                case 1:
+                    itemList.add(new Item("Super Potion", "super_potion", "Restaure 50 PV", 1));
+                    break;
+                case 2:
+                    itemList.add(new Item("Hyper Potion", "hyper_potion", "Restaure 200 PV", 1));
+                    break;
+            }
+        }
+        return itemList;
     }
 
     public String getName() {
