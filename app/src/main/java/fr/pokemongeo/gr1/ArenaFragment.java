c453379f1a3270c03ef7adb82f018620bd783a25
maximecornaton <maxime.cornaton@cpe.fr>
2023-11-05 15:23:23 +0100

    arena

---
+++ app/src/main/java/fr/pokemongeo/gr1/ArenaFragment.java
@@ -0,0 +1,52 @@
+package fr.pokemongeo.gr1;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import fr.pokemongeo.gr1.databinding.ArenaFragmentBinding;
+
+public class ArenaFragment extends Fragment {
+    private ArenaFragmentBinding binding;
+    public ArenaFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater,
+                R.layout.pokestop_fragment, container, false);
+        View rootView = binding.getRoot();
+
+
+
+        MaterialButton backButtonMaterial = rootView.findViewById(R.id.backButton);
+        // Ajouter un listener de clic au bouton
+        backButtonMaterial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retourner au fragment précédent
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                fragmentManager.popBackStack();
+            }
+        });
+
+        return rootView;
+    }
+
+
+}
