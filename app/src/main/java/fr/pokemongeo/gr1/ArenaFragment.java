f6a50c58a95e31cb98fc1889014f7b5631ffe07e
Adrien Chausson <adrien.chausson@hotmail.fr>
2023-11-07 20:21:30 +0100

    design

--- app/src/main/java/fr/pokemongeo/gr1/ArenaFragment.java
+++ app/src/main/java/fr/pokemongeo/gr1/ArenaFragment.java
@@ -1,7 +1,5 @@
 package fr.pokemongeo.gr1;
 
-import android.content.ContentValues;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,13 +8,8 @@ import android.view.ViewGroup;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.button.MaterialButton;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 import fr.pokemongeo.gr1.databinding.ArenaFragmentBinding;
 
@@ -31,7 +24,6 @@ public class ArenaFragment extends Fragment {
         binding = DataBindingUtil.inflate(inflater,
                 R.layout.arena_fragment, container, false);
         View rootView = binding.getRoot();
-
 
 
         MaterialButton backButtonMaterial = rootView.findViewById(R.id.backButton);
